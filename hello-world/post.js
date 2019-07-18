8af5f5f03eb91555fbf915fbe363c5e38b0c7944
Ubuntu <ubuntu@ip-172-31-90-184.ec2.internal>
2019-07-18 07:00:14 +0000

    added new files

---
+++ hello-world/post.js
@@ -0,0 +1,20 @@
+
+let response;
+
+exports.handler = async (event, context) => {
+    console.log(event.body) // printing the entire payload received...
+    try {
+	payload = JSON.parse(event.body)  // event is coming as JSON, so let's parse it so we can access it as a JS object
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: payload.message
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
