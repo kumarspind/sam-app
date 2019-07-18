be2750ffa9e661fbe740a78ac8af8847784dca40
Ubuntu <ubuntu@ip-172-31-90-184.ec2.internal>
2019-07-18 07:25:08 +0000

    code fix'

--- hello-world/post.js
+++ hello-world/post.js
@@ -6,9 +6,9 @@ exports.handler = async (event, context) => {
 	payload = JSON.parse(event.body)  // event is coming as JSON, so let's parse it so we can access it as a JS object
         response = {
             'statusCode': 200,
-            'body': JSON.stringify({
+            'body': JSON.parse(JSON.stringify({
                 message: payload.message
-            })
+            }))
         }
     } catch (err) {
         console.log(err);
