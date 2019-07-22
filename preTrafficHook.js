6c216bcf62aebf0bc40b6739c72665a8cc4a21b5
kumarspind <52922179+kumarspind@users.noreply.github.com>
2019-07-22 11:03:51 +0530

    Update preTrafficHook.js

--- preTrafficHook.js
+++ preTrafficHook.js
@@ -37,7 +37,7 @@ exports.handler = (event, context, callback) => {
 			//		"statusCode": 200,
 			//		"body": 51
 			// }
-			if(result.body == 11){	
+			if(result.body == 6){	
 				lambdaResult = "Succeeded";
 				console.log ("Validation testing succeeded!");
 			}
