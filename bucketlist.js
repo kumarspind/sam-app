5a071ecbb5265f4ca196163a9fce5b626d486bc8
kumarspind <52922179+kumarspind@users.noreply.github.com>
2019-07-30 17:29:33 +0530

    Update bucketlist.js

--- bucketlist.js
+++ bucketlist.js
@@ -17,7 +17,7 @@ exports.handler = (event, context, callback) => {
 		}
 		else{
 			var allBuckets = data.Buckets;
-			console.log("XXXXXXXXXXXXX");
+			console.log("XXXWORLDXXX");
 			console.log("Total bucket count is: " + allBuckets.length);
 			callback(null, {
 				statusCode: 200,
